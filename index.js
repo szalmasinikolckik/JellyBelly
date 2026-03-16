b3080ce14e2b19aece66db6647129ec483b713f2
fulep.fanni <fulep.fanni@ckik.hu>
2026-03-16 09:26:16 +0100

    A napi jellybelly elkezdese (nincs eleg belly igy meg nem a legjobb )

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const API = "https://jellybellywikiapi.onrender.com/api/beans";
 
 let beans = [];
-let guessBean = null;
 
 async function loadBeans() {
     const res = await fetch(API);
     const data = await res.json();
     beans = data.items;
+
+    console.log("Összes íz betöltve:", beans.length);
 }
 
 loadBeans();
 
 
-console.log(beans.length)
 document.getElementById("randomBtn").addEventListener("click", () => {
-    
+    if (beans.length === 0) return alert("Még töltődnek az ízek!");
+
     const bean = beans[Math.floor(Math.random() * beans.length)];
     document.getElementById("randomResult").innerHTML = `
-    <h3>${bean.flavorName}</h3>
-    <p>${bean.description}</p>
-    <img src="${bean.imageUrl}" width="120">
+        <h3>${bean.flavorName}</h3>
+        <p>${bean.description}</p>
+        <img src="${bean.imageUrl}" width="120">
     `;
 });
 
+document.getElementById("dailyBtn").addEventListener("click", () => {
 
+    if (beans.length === 0) return alert("Még töltődnek az ízek!");
+
+   
+    const today = new Date();
+
+    
+    const dayNumber = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+
+
+    const bean = beans[dayNumber % beans.length];
+
+    document.getElementById("dailyResult").innerHTML = `
+        <h3>${bean.flavorName}</h3>
+        <p>${bean.description}</p>
+        <img src="${bean.imageUrl}" width="120">
+    `;
+});
