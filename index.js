e800dbd7a703040078348af8d1fe413b93f07839
szalmasi.nikol <szalmasi.nikol@ckik.hu>
2026-03-20 09:23:40 +0100

    nem tudom

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const API = "https://jellybellywikiapi.onrender.com/api/beans";
 
 let beans = [];
-let guessBean = null;
 
 async function loadBeans() {
-    const res = await fetch(API);
-    const data = await res.json();
-    beans = data.items;
+    const requests = [];
+    for (let i = 0; i < 10; i++) {
+        requests.push(fetch(`${API}?page=${i}`).then(res => res.json()));
+        
+    }
+    const results = await Promise.all(requests);
+    beans = results.flatMap(data => data.beans)
 }
+
 
 loadBeans();
 
 
-console.log(beans.length)
 document.getElementById("randomBtn").addEventListener("click", () => {
-    
+    alert(beans.length);
     const bean = beans[Math.floor(Math.random() * beans.length)];
     document.getElementById("randomResult").innerHTML = `
     <h3>${bean.flavorName}</h3>
